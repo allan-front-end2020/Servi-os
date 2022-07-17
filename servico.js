b5e1f4168c69b0f0ebd941787675aea780223ac8
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-07-16 22:50:49 -0300

    sessão de conhecimentos finalizado

---
+++ servico.js
@@ -0,0 +1,6 @@
+var hanburgue = document.querySelector('.hanburgue');
+
+hanburgue.addEventListener('click', function() {
+    document.querySelector('.container').classList.toggle('show-menu')
+
+});
